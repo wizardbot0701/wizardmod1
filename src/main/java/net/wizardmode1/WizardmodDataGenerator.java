6600afc74c88ff3a5975e47d5fd2658591fee96a
wizardbot0701 <wizardbot102@gmail.com>
2025-03-22 16:46:52 +0900

    일반 기초 구축 완료

---
+++ src/main/java/net/wizardmode1/WizardmodDataGenerator.java
@@ -0,0 +1,11 @@
+package net.wizardmode1;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WizardmodDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
